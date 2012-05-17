fa10d7fdecfc9927bcf9c3e523ef4a20d446bb9f
Ash Ward <ash@netality.com>
2012-05-17 14:23:51 +0100

    Fixed viewing other users' profiles

--- src/main/java/com/calclab/hablar/vcard/client/OtherVCardWidget.java
+++ src/main/java/com/calclab/hablar/vcard/client/OtherVCardWidget.java
@@ -1,13 +1,64 @@
 package com.calclab.hablar.vcard.client;
 
+import com.google.gwt.event.dom.client.HasClickHandlers;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HasText;
 import com.google.gwt.user.client.ui.ScrollPanel;
 
-public class OtherVCardWidget extends VCardWidget implements VCardDisplay {
+public class OtherVCardWidget extends Composite implements VCardDisplay {
 
+	private VCardWidget vCardWidget;
+	
 	public OtherVCardWidget(final boolean readOnly) {
 		final ScrollPanel scroll = new ScrollPanel();
-		scroll.add(uiBinder.createAndBindUi(this));
-		init(readOnly, "OtherVCardWidget");
+		vCardWidget = new VCardWidget(readOnly, "OtherVCardWidget");
+		scroll.add(vCardWidget);
+		initWidget(scroll);
+	}
+
+	@Override
+	public HasClickHandlers getCancel() {
+		return vCardWidget.getCancel();
+	}
+
+	@Override
+	public HasText getField(Field field) {
+		return vCardWidget.getField(field);
+	}
+
+	@Override
+	public HasText getLoading() {
+		return vCardWidget.getLoading();
+	}
+
+	@Override
+	public void setAcceptVisible(boolean visible) {
+		vCardWidget.setAcceptVisible(visible);	
+	}
+
+	@Override
+	public void setCancelText(String text) {
+		vCardWidget.setCancelText(text);		
+	}
+
+	@Override
+	public void setCancelVisible(boolean visible) {
+		vCardWidget.setCancelVisible(visible);
+	}
+
+	@Override
+	public void setFormVisible(boolean visible) {
+		vCardWidget.setFormVisible(visible);
+	}
+
+	@Override
+	public void setLoadingVisible(boolean visible) {
+		vCardWidget.setLoadingVisible(visible);
+	}
+
+	@Override
+	public void setPageTitle(String title) {
+		vCardWidget.setPageTitle(title);
 	}
 
 }
